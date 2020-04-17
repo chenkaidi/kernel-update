184407f7721afb3f94eed9f15943f712e02a6911
向上的信念 <19582627+xsdxn@users.noreply.github.com>
2020-04-17 15:53:15 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@
 
 #centos7 升级内核
 
-
 yum -y update
 
 #启用 ELRepo 仓库：
@@ -20,8 +19,6 @@ yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
 #或者，安装长期支持版内核: kernel-lt
 yum --enablerepo=elrepo-kernel install kernel-lt -y
 
-
-
 #设置 grub2
 #通过 grub2-set-default 0 命令设置：
 #其中 0 来自上一步的 awk 命令：
@@ -31,9 +28,8 @@ grub2-set-default 0
 #设置 GRUB_DEFAULT=0
 sed -i '/GRUB_DEFAULT/s/^.*$/GRUB_DEFAULT=0/g' /etc/default/grub
 
-
 #生成 grub 配置文件并重启
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
 #重启
-#reboot
+reboot
